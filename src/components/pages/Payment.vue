81f5574ab86b64421d44b36513bb768e51d05bfe
Nhat2022 <nhatbui2017@gmail.com>
2022-08-02 08:43:19 +0700

    Update Payment

--- src/components/pages/Payment.vue
+++ src/components/pages/Payment.vue
@@ -16,82 +16,29 @@
                                     </tr>
                                 </thead>
                                 <tbody>
-                                    <tr>
+                                    <tr v-for="(item, index) in dataCart" :key="index">
                                         <td class="shoping__cart__item">
                                             <img src="../../assets/images/unsplash_60nzTP7_hMQ.png" alt="">
                                             <div class="shoping__cart__item-header">
-                                                <h4>Home made pizza 12’</h4>
-                                                <h5>beef patties, Iceberg lettuce, American cheese, pickles, ...</h5>
+                                                <h4>{{item.name}}</h4>
+                                                <h5>{{item.description}}</h5>
                                             </div>
                                         </td>
                                         <td class="shoping__cart__price">
-                                            $55.00
+                                            {{item.afterPrice}}$
                                         </td>
                                         <td class="shoping__cart__quantity">
                                             <div class="quantity">
-                                                <div class="quantity_minus" @click="uncreament()">-</div>
+                                                <div class="quantity_minus" @click="uncreament(item.id)">-</div>
                                                 <div class="pro-qty">
-                                                    <input type="text" v-model="count">
+
+                                                    <input type="tet" v-model="dataCart[index].quantity">
                                                 </div>
-                                                <div class="quantity_plus" @click="increament()">+</div>
+                                                <div class="quantity_plus" @click="increament(item.id)">+</div>
                                             </div>
                                         </td>
                                         <td class="shoping__cart__total">
-                                            $110.00
-                                        </td>
-                                        <td class="shoping__cart__item__close">
-                                            <span class="icon_close"></span>
-                                        </td>
-                                    </tr>
-                                    <tr>
-                                        <td class="shoping__cart__item">
-                                            <img src="../../assets/images/unsplash_MqT0asuoIcU.png" alt="">
-                                            <div class="shoping__cart__item-header">
-                                                <h4>Home made pizza 12’</h4>
-                                                <h5>beef patties, Iceberg lettuce, American cheese, pickles, ...</h5>
-                                            </div>
-                                        </td>
-                                        <td class="shoping__cart__price">
-                                            $39.00
-                                        </td>
-                                        <td class="shoping__cart__quantity">
-                                            <div class="quantity">
-                                                <div class="quantity_minus" @click="uncreament()">-</div>
-                                                <div class="pro-qty">
-                                                    <input type="text" v-model="count">
-                                                </div>
-                                                <div class="quantity_plus" @click="increament()">+</div>
-                                            </div>
-                                        </td>
-                                        <td class="shoping__cart__total">
-                                            $39.99
-                                        </td>
-                                        <td class="shoping__cart__item__close">
-                                            <span class="icon_close"></span>
-                                        </td>
-                                    </tr>
-                                    <tr>
-                                        <td class="shoping__cart__item">
-                                            <img src="../../assets/images/unsplash_33GPuoFI7v8.png" alt="">
-                                            <div class="shoping__cart__item-header">
-                                                <h4>Home made pizza 12’</h4>
-                                                <h5>beef patties, Iceberg lettuce, American cheese, pickles, ...</h5>
-                                            </div>
-                                        </td>
-                                        <td class="shoping__cart__price">
-                                            $69.00
-                                        </td>
-                                        <td class="shoping__cart__quantity">
-                                            <div class="quantity">
-                                                <div class="quantity_minus" @click="uncreament()">-</div>
-                                                <div class="pro-qty">
-                                                    <input type="text" v-model="count">
-                                                </div>
-                                                <div class="quantity_plus" @click="increament()">+</div>
-                                            </div>
-                                        </td>
-                                        <td class="shoping__cart__total">
-                                            $69.99
+                                            {{item.price}}$
                                         </td>
                                         <td class="shoping__cart__item__close">
                                             <span class="icon_close"></span>
@@ -125,45 +72,157 @@
                         <div class="shoping__checkout">
                             <h5>Total amount in cart</h5>
                             <ul>
-                                <li>Subtotal <span>$4980</span></li>
+                                <li>Subtotal  <span> ${{subtotal}}</span></li>
                                 <li>Shipping fee <span>$20</span></li>
                                 <li>Coupon <span>0</span></li>
-                                <li>TOTAL:  <span>5000</span></li>
+                                <li>TOTAL:  <span>${{subtotal-20}}</span></li>
                             </ul>
-                            <a href="#" class="primary-btn">PAY ALL: 5000</a>
+                            <a href="#" data-toggle="modal" data-target="#exampleModalLong" class="primary-btn">PAY ALL: ${{subtotal-20}}</a>
                         </div>
                     </div>
                 </div>
             </div>
         </section>
-    </div>
+        <!-- Modal Payment -->
+        <div class="modal fade" id="exampleModalLong" tabindex="-1" role="dialog" aria-labelledby="exampleModalLongTitle" aria-hidden="true">
+          <div class="modal-dialog" role="document">
+            <div class="modal-content">
+              <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLongTitle">Modal title</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                  <span aria-hidden="true">&times;</span>
+                </button>
+              </div>
+              <div class="modal-body">
+                <div class="container">
+                  <div class="progress-container">
+                    <div class="progress" id="progress"></div>
+                    <div class="circle active">1</div>
+                  <div class="circle ">2</div>
+                  <div class="circle ">3</div>
+                  </div>
+                </div>
+                <form class="payment__form">
+                  <div class="form-row">
+                    <div class="form-group col-md-6">
+                      <input type="email" class="form-control" id="inputEmail4" placeholder="Email">
+                    </div>
+                    <div class="form-group col-md-6">
+                      <input type="password" class="form-control" id="inputPassword4" placeholder="Password">
+                    </div>
+                  </div>
+                  <div class="form-row">
+                    <div class="form-group col-md-6">
+                      <input type="email" class="form-control" id="inputEmail4" placeholder="Email">
+                      <h5 class="pt-2 pb-2 text-left">Select Method of Payment</h5>
+                      <ul class="list-group border-0">
+                        <li class="list-group-item d-flex justify-content-between align-items-center">
+                          Cras justo odio
+                          <span><input type="checkbox" class="custom-control-input" id="customControlInline"></span>
+                        </li>
+                        <li class="list-group-item d-flex justify-content-between align-items-center">
+                          Dapibus ac facilisis in
+                          <span class="badge badge-primary badge-pill">2</span>
+                        </li>
+                        <li class="list-group-item d-flex justify-content-between align-items-center">
+                          Morbi leo risus
+                          <span class="badge badge-primary badge-pill">1</span>
+                        </li>
+                      </ul>
+                    </div>
+                    <div class="form-group col-md-6">
+                      <input type="password" class="form-control address" id="inputPassword4" placeholder="Password">
+                      <div class="form-group pt-3">
+                        <input type="text" class="form-control" id="inputAddress" placeholder="1234 Main St">
+                      </div>
+                    </div>
+                  </div>
+                </form>
+              </div>
+              <div class="modal-footer">
+                <button type="button" class="btn btn-default mx-auto">Go to Payment</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
 </template>
 
-<script>    
+<script>
 export default {
 		data() {
-		return {
-			count: 1,
-		};
+      return {
+        count: 1,
+        dataCart :[
+					{
+            id: 1,
+						image: "https://drive.google.com/drive/folders/1lJTnCbfcppROLQbGfsc3aLW15yQCIU8w",
+						name: "Home made pizza 12’",
+            quantity: 1,
+						price: 10,
+            afterPrice: 10,
+						description: 'beef patties, Iceberg lettuce, American cheese, pickles, ...'
+					},
+          {
+            id: 2,
+						image: "https://drive.google.com/drive/folders/1lJTnCbfcppROLQbGfsc3aLW15yQCIU8w",
+						name: "Home made pizza 12’",
+            quantity: 1,
+            price: 12,
+						afterPrice: 12,
+						description: 'beef patties, Iceberg lettuce, American cheese, pickles, ...'
+					},
+          {
+            id: 3,
+						image: "https://drive.google.com/drive/folders/1lJTnCbfcppROLQbGfsc3aLW15yQCIU8w",
+						name: "Home made pizza 12’",
+            quantity: 1,
+						price: 15,
+            afterPrice: 15,
+						description: 'beef patties, Iceberg lettuce, American cheese, pickles, ...'
+					}
+        ],
+        subtotal : 0
+      };
 		},
-		computed: {},
+    mounted(){
+      this.SubtotalPlus();
+    },
+		computed: {
+    },
 		methods: {
-			increament(){
-                if(this.count>=1){
-                    this.count++;
+			increament(id){
+                var objIndex = this.dataCart.findIndex((obj => obj.id == id));// Tìm kiếm id của món ăn
+                if(this.dataCart[objIndex].quantity>=1){
+                    this.dataCart[objIndex].quantity++;
+                    this.dataCart[objIndex].afterPrice = this.dataCart[objIndex].price * this.dataCart[objIndex].quantity;
+                    this.SubtotalPlus();
                 }
-            },
-            uncreament(){
-                if(this.count>1){
-                    this.count--;
+      },
+      uncreament(id){
+                var objIndex = this.dataCart.findIndex((obj => obj.id == id));// Tìm kiếm id của món ăn
+                if(this.dataCart[objIndex].quantity>1){
+                    this.dataCart[objIndex].quantity--;
+                    this.dataCart[objIndex].afterPrice=this.dataCart[objIndex].price * this.dataCart[objIndex].quantity;
+                    this.SubtotalPlus();
                 }
-            },
+      },
+      SubtotalPlus(){
+        this.subtotal = 0;
+        for (let index = 0; index < this.dataCart.length; index++) {
+          this.subtotal += this.dataCart[index].price * this.dataCart[index].quantity;
+        }
+      }
 		},
 	};
 </script>
 
 <style>
-    .shoping-cart {
+.btn-default{
+  background-color: var(--primary-color);
+  color: var(--white-color);
+}
+.shoping-cart {
 	padding-top: 80px;
 	padding-bottom: 80px;
 }
@@ -414,5 +473,76 @@ export default {
     border-radius: 10px;
     margin: auto;
     width: 340px;
+}
+:root{
+    --line-fill: #1AC073;
+    --line-empty: #e0e0e0;
+}
+
+.progress-container {
+  width: 150px;
+  display: flex;
+  max-width: 100%;
+  justify-content: space-between;
+  position: relative;
+  margin: 30px auto;
+}
+.progress-container::before {
+  content: '';
+  position: absolute;
+  top: 50%;
+  left: 0;
+  background-color: #DFDEDE;
+  height: 2px;
+  width: 100%;
+  transform: translateY(-50%);
+  z-index: 0;
+}
+.progress {
+  position: absolute;
+  top: 50%;
+  left: 0;
+  background-color: var(--line-fill);
+  height: 4px;
+  width: 0%;
+  transform: translateY(-50%);
+  z-index: -1;
+  transition: 0.4s all ease;
+}
+
+.circle {
+  height: 30px;
+  width: 30px;
+  background-color: var(--line-empty);
+  border-radius: 50%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border: 3px solid var(--line-empty);
+   transition: 0.4s ease;
+   z-index: 1;
+
+}
+.circle.active {
+  background: #1AC073;
+  color: #fff;
+  border: 0px;
+}
+button:disabled {
+  cursor: not-allowed;
+  background-color: var(--line-empty);
+}
+@media (min-width: 576px){
+  .modal-dialog {
+      max-width: 650px;
+      margin: 1.75rem auto;
+  }
+  .payment__form input {
+  background: #DFDEDE;
+  height: 50px;
+  }
+  .payment__form .address{
+    height: 135px;
+  }
 }
 </style>
